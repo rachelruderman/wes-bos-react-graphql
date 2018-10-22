4f6a376a9139b4c3d3b85dbe60db582a88c0a32e
Rachel Ruderman <rachel@yembo.ai>
2018-10-22 14:13:24 -0700

    Add pagination to queried items

--- sick-fits/frontend/components/Items.js
+++ sick-fits/frontend/components/Items.js
@@ -4,6 +4,7 @@ import gql from 'graphql-tag';
 import styled from 'styled-components';
 import Item from './Item';
 import Pagination from './Pagination';
+import {perPage} from '../config';
 
 const Center = styled.div`
     text-align: center;
@@ -18,8 +19,8 @@ const ItemsList = styled.div`
 `;
 
 export const ALL_ITEMS_QUERY = gql`
-    query ALL_ITEMS_QUERY {
-        items {
+    query ALL_ITEMS_QUERY($skip: Int = 0, $first: Int = ${perPage}) {
+        items(first: $first, skip: $skip, orderBy: createdAt_DESC){
             id
             title
             price
@@ -32,22 +33,26 @@ export const ALL_ITEMS_QUERY = gql`
 
 export default class Items extends Component {
   render() {
+    const {page} = this.props;
     return (
       <Center>
-          <Pagination page={this.props.page}/>
-            <Query query={ALL_ITEMS_QUERY}>
+          <Pagination page={page}/>
+            <Query query={ALL_ITEMS_QUERY} variables={{
+                skip: (page * perPage) - perPage,
+                first: 4
+            }}>
                 {({data, error, loading}) => {
                     if (loading) return <p>Loading...</p>;
                     if (error)   return <p>Error: {error.message}</p>
 
                     return (
-                        <ItemsList page={this.props.page}>
+                        <ItemsList>
                             {data.items.map( (item) => <Item key={item.id} item={item}/>)}
                         </ItemsList>
                     )
                 }}
             </Query>
-        <Pagination page={this.props.page}/>
+        <Pagination page={page}/>
       </Center>
     )
   }
