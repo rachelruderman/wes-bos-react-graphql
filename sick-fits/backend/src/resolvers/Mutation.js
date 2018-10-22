13030ff0bf4a84b66f1e8eff236cd2c738e5661f
Rachel Ruderman <rachel@yembo.ai>
2018-10-22 10:30:44 -0700

    Delete Item calls

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -10,18 +10,28 @@ const Mutations = {
     },
 
     updateItem(parent, args, ctx, info){
-    //first take copy of hte updates
-    const updates = {...args};
-    // remove the ID from the updates
-    delete updates.id;
-    // run the update method
-    return ctx.db.mutation.updateItem({
-        data: updates,
-        where: {
-            id: args.id
-        }
-        //info is how it knows what to return to us
-    }, info);
+        //first take copy of hte updates
+        const updates = {...args};
+        // remove the ID from the updates
+        delete updates.id;
+        // run the update method
+        return ctx.db.mutation.updateItem({
+            data: updates,
+            where: {
+                id: args.id
+            }
+            //info is how it knows what to return to us
+        }, info);
+    },
+
+    async deleteItem(parent, args, ctx, info){
+        const where = {id: args.id};
+        // (1) find the item
+        const item = await ctx.db.query.item({where}, `{id title}`)
+        // (2) check if they own that item or have the permissions
+        // TODO
+        // (3) delete it
+        return ctx.db.mutation.deleteItem({where}, info)
     }
 };
 
