773bd1bb61ead47bde16d70ecf57bc21cca941a4
Rachel Ruderman <rachel@yembo.ai>
2018-10-22 14:46:06 -0700

    Add sign up

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs');
+const jwt    = require('jsonwebtoken');
+
 const Mutations = {
     async createItem(parent, args, ctx, info){
         //Todo: check if they are logged in
@@ -7,6 +10,16 @@ const Mutations = {
             }
         }, info);
         return item;
+    },
+
+    async deleteItem(parent, args, ctx, info){
+        const where = {id: args.id};
+        // (1) find the item
+        const item = await ctx.db.query.item({where}, `{id title}`)
+        // (2) check if they own that item or have the permissions
+        // TODO
+        // (3) delete it
+        return ctx.db.mutation.deleteItem({where}, info)
     },
 
     updateItem(parent, args, ctx, info){
@@ -24,14 +37,28 @@ const Mutations = {
         }, info);
     },
 
-    async deleteItem(parent, args, ctx, info){
-        const where = {id: args.id};
-        // (1) find the item
-        const item = await ctx.db.query.item({where}, `{id title}`)
-        // (2) check if they own that item or have the permissions
-        // TODO
-        // (3) delete it
-        return ctx.db.mutation.deleteItem({where}, info)
+    async signUp(parent, args, ctx, info){
+        const email     = args.email.toLowerCase();
+        const password  = await bcrypt.hash(args.password, 32);
+
+        const user      = await ctx.db.mutation.createUser({
+            data: {
+                ...args, 
+                email, 
+                password, 
+                permissions: {set: ['USER']}
+            }
+        }, info)
+
+        //create the JWT token for them
+        const token = jwt.sign({userId: user.id}, process.env.APP_SECRET);
+        // We set the jwt as a cookie on the response
+        ctx.response.cookie('token', token, {
+            httpOnly:   true,
+            maxAge:     (1000 * 60 * 60 * 24 * 365) // 1 year cookie
+        });
+        // Finally we return the user to the browser
+        return user;
     }
 };
 
